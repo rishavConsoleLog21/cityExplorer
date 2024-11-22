9ae1daed14aa50703be9d85abe878bd41714d28e
rishavConsoleLog21 <kumarrishavcool21@gmail.com>
2024-11-22 16:00:43 +0530

    Add myTrip UI

---
+++ src/components/MyTrips/StartNewTripCard.tsx
@@ -0,0 +1,55 @@
+import {Pressable, StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import Icon from 'react-native-vector-icons/FontAwesome5';
+
+const StartNewTripCard = () => {
+  return (
+    <View
+      style={{
+        padding: 20,
+        marginTop: 50,
+        display: 'flex',
+        alignItems: 'center',
+        gap: 15,
+      }}>
+      <Icon name="map-marked-alt" size={35} color="#38CC77" />
+      <Text
+        style={{
+          fontSize: 25,
+          fontWeight: 'medium',
+          textAlign: 'center',
+          color: 'black',
+        }}>
+        No Trip planned yet!
+      </Text>
+      <Text
+        style={{
+          fontSize: 15,
+          textAlign: 'center',
+          color: 'gray',
+        }}>
+        Looks like time to plan a new trip. Click the button below to start a
+        new trip.
+      </Text>
+      <Pressable
+        style={{
+          padding: 10,
+          borderRadius: 10,
+          backgroundColor: '#38CC77',
+        }}>
+        <Text
+          style={{
+            fontSize: 20,
+            color: 'white',
+            backgroundColor: '#38CC77',
+          }}>
+          Start New Trip
+        </Text>
+      </Pressable>
+    </View>
+  );
+};
+
+export default StartNewTripCard;
+
+const styles = StyleSheet.create({});
--- src/screens/tabs/Mytrips.tsx
+++ src/screens/tabs/Mytrips.tsx
@@ -1,10 +1,37 @@
 import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import React, {useState} from 'react';
+import {Colors} from '../../constants/Colors';
+import Icon from 'react-native-vector-icons/FontAwesome5';
+import StartNewTripCard from '../../components/MyTrips/StartNewTripCard';
 
 const Mytrips = () => {
+  const [userTrips, setUserTrips] = useState([]);
+
   return (
-    <View>
-      <Text>Mytrips</Text>
+    <View
+      style={{
+        padding: 25,
+        paddingTop: 50,
+        backgroundColor: Colors.Secondary,
+        height: '100%',
+      }}>
+      <View
+        style={{
+          flexDirection: 'row',
+          alignContent: 'center',
+          justifyContent: 'space-between',
+        }}>
+        <Text
+          style={{
+            color: Colors.Black,
+            fontSize: 30,
+            fontWeight: 'bold',
+          }}>
+          My Trips
+        </Text>
+        <Icon name="plus-circle" size={35} color="#38CC77" />
+      </View>
+      {userTrips.length == 0 ? <StartNewTripCard /> : null}
     </View>
   );
 };
